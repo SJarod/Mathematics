733f3061873ffec992b463cd577569d64a1f7fc5
Jarod Sengkeo <jarsengkeoPro@gmail.com>
2023-01-17 09:58:36 +0100

    commencing ann implementation

--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,61 @@
 
 int main()
 {
-	std::cout << "Hello world !" << std::endl;
-
-	vec<mat4, 2> v = { mat4::id, mat4::zo };
-
-	std::cout << v << std::endl;
-
-	vec<double, 4> d = { 1.f, 2, 3.f, 4.f };
-
-	std::cout << d << std::endl;
-
-	class Test
+	// mathematics
 	{
-	private:
-		int a;
-	public:
-		Utils::Property<int> p{
-			[this]() -> const int& { return a; },
-			[this](const int& i) { a = i; }
-		};
-		Utils::Property<int> p2{
-			[this]() -> const int& { return a; },
-			nullptr
-		};
-	};
+		std::cout << "Hello world !" << std::endl;
 
-	Test t;
-	t.p2 = 5;
-	std::cout << t.p << std::endl;
+		vec<mat4, 2> v = { mat4::id, mat4::zo };
 
-	Utils::AI::NeuralNetwork ann;
+		std::cout << v << std::endl;
+
+		vec<double, 4> d = { 1.f, 2, 3.f, 4.f };
+
+		std::cout << d << std::endl;
+	}
+
+	// property
+	{
+		class Test
+		{
+		private:
+			int a;
+		public:
+			Utils::Property<int> p{
+				[this]() -> const int& { return a; },
+				[this](const int& i) { a = i; }
+			};
+			Utils::Property<int> p2{
+				[this]() -> const int& { return a; },
+				nullptr
+			};
+		};
+
+		Test t;
+		std::cout << t.p << std::endl;
+		//t.p2 = 5;
+	}
+
+	// artificial neural network
+	{
+		std::vector<float> inputs;
+		inputs.push_back(0);
+		inputs.push_back(0);
+
+		int hiddenLayerNum = 2;
+		int outputNum = 1;
+		Utils::AI::NeuralNetwork ann = { hiddenLayerNum, outputNum };
+
+		for (;;)
+		{
+			ann.feedForward(inputs);
+			std::vector<float> outputs = ann.getOutputs();
+
+			std::cout << outputs[0] << std::endl;
+
+			ann.backPropagation();
+		}
+	}
 
 	return 0;
 }
