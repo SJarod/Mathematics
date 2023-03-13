fe07ff36f00bc77a51272dfae301ce864517d2b5
Jarod Sengkeo <jarsengkeoPro@gmail.com>
2023-03-13 16:33:10 +0100

    queue test

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 
 #include "utils/derived.hpp"
 
+#include "utils/containers/queue.hpp"
+
 int main()
 {
 	CHKLK_INIT
@@ -276,6 +278,28 @@ int main()
 
 		C c;
 		c.foo();
+	}
+
+	// queue test
+	{
+		Utils::Queue<int> myQueue;
+		myQueue.pushBackNode(0);
+		myQueue.pushBackNode(1);
+		myQueue.pushBackNode(2);
+		myQueue.pushBackNode(3);
+		myQueue.pushBackNode(4);
+
+		std::cout << myQueue.frontData() << std::endl;
+		std::cout << myQueue.frontData(1) << std::endl;
+		std::cout << myQueue.frontData(2) << std::endl;
+		std::cout << myQueue.frontData(3) << std::endl;
+		std::cout << myQueue.frontData(4) << std::endl;
+		//std::cout << myQueue.frontData(5) << std::endl; // out of range (assert)
+	}
+
+	// threadpool test
+	{
+
 	}
 
 	CHKLK_EXIT_SNAP
