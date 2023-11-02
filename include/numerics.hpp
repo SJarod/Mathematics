e815f195db3232d158f89438b881fbf5ff5e04ee
SJarod <91637364+SJarod@users.noreply.github.com>
2023-11-02 22:41:56 +0100

    Update numerics.hpp

--- include/numerics.hpp
+++ include/numerics.hpp
@@ -1,5 +1,31 @@
 #pragma once
 
-#include <stdint.h>
+//#define STD_INT
+#ifdef STD_INT
 
-typedef unsigned int uint; // uint32_t
+#include <cstdint>
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+#else
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef signed long long int64;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+#endif
+
+typedef bool uint1;
